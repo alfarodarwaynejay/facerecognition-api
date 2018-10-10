c5d81a449fab5a6c21867361c34f3c455b0372ba
purplebyte <alfarodarwayne@gmail.com>
2018-10-10 09:10:42 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+//controllers
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+
+//end-point call
+app.post('/signin', (req, res) => {signin.handleSignInPost(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {register.handleRegisterPost(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+app.put('/image', (req,res) => {image.handleImagePut(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
